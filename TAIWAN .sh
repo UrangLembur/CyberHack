14b19ec942b55d2ddf64c5f064018adb6260b2e5
UrangLembur <61285532+UrangLembur@users.noreply.github.com>
2020-02-21 13:19:36 +0700

    Add files via upload

---
+++ TAIWAN .sh
@@ -0,0 +1,93 @@
+echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+echo "ㅤㅤㅤㅤㅤ ㅤㅤCreator ™ Teratai Bi®u
+echo "ㅤㅤㅤㅤㅤㅤㅤ project_CyberLembur ▶ hack pubg S11 ◀"
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+am start -n com.rekoo.pubgm/com.epicgames.ue4.SplashActivity
+sleep 7
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+dd if=/dev/urandom of=/storage/emulated/0/project_CyberLembur/libUE4.so bs=1k count=5000
+dd if=/dev/urandom of=/storage/emulated/0/project_CyberLembur/libtersafe.so bs=1k count=4000
+dd if=/dev/urandom of=/storage/emulated/0/project_CyberLembur/libTDataMaster.so bs=1k count=3000
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 544 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/core_patch_0.16.0.11462.pak
+chmod -R 544 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/core_patch_0.16.5.11560.pak
+chmod -R 544 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.0.11463.pak
+chmod -R 544 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.0.11464.pak
+chmod -R 544 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.16.0.11466.pak
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 544 /data/media/0/Android/obb/com.rekoo.pubgm/main.11460.com.rekoo.pubgm.obb
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 000 /data/media/0/Android/data/com.rekoo.pubgm/cache
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 000 /data/media/0/Android/data/com.rekoo.pubgm/files/ca-bundle.pem
+chmod -R 000 /data/media/0/Android/data/com.rekoo.pubgm/files/cacheFile.txt
+chmod -R 000 /data/media/0/Android/data/com.rekoo.pubgm/files/login-identifier.txt
+chmod -R 000 /data/media/0/Android/data/com.rekoo.pubgm/files/vmpcloudconfig.json
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 000 /data/media/0/Android/data/com.rekoo.pubgm/files/tbslog
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 000 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 544 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist
+chmod -R 544 /data/media/0/Android/data/com.rekoo.pubgm/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 000 /data/user/0/com.rekoo.pubgm/app_webview_imsdk_inner_webview
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 000 /data/user/0/com.rekoo.pubgm/cache
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+chmod -R 000 /data/user/0/com.rekoo.pubgm/databases
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+mv /data/user/0/com.rekoo.pubgm/lib/{libTDataMaster.so,ORI_01}
+mv /data/user/0/com.rekoo.pubgm/lib/{libtersafe.so,ORI_02}
+mv /data/user/0/com.rekoo.pubgm/lib/{libUE4.so,ORI_03}
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+mv /storage/emulated/0/project_CyberLembur/libTDataMaster.so /data/user/0/com.rekoo.pubgm/lib
+mv /storage/emulated/0/project_CyberLembur/libtersafe.so /data/user/0/com.rekoo.pubgm/lib
+mv /storage/emulated/0/project_CyberLembur/libUE4.so /data/user/0/com.rekoo.pubgm/lib
+chmod -R 755 /data/user/0/com.rekoo.pubgm/lib/libTDataMaster.so
+chmod -R 755 /data/user/0/com.rekoo.pubgm/lib/libtersafe.so
+chmod -R 755 /data/user/0/com.rekoo.pubgm/lib/libUE4.so
+# Creator ™ Teratai bi®u
+# hasil karya anak kampung
+# Tangerang
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+echo "ㅤㅤㅤㅤㅤ ㅤㅤCreator ™ Teratai Bi®u
+echo "ㅤㅤㅤㅤㅤㅤㅤ project_CyberLembur▶ hack pubg S11 ◀"
+echo " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+echo "▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
